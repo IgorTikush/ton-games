e39f3c0f554a864040ca1e755fb620140ba84479
Igor GoLogin <igor@easync.io>
2024-06-04 01:20:27 +1000

    config change

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://c6bd-194-195-245-224.ngrok-free.app';
+export const API_URL = 'https://5066-194-195-245-224.ngrok-free.app';
 // export const API_URL = 'http://localhost:3000';
